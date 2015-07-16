76fa4e986514470fbba628f6ab427ebe921095e5
hoarh <hoarh@hannahs-MacBook-Pro-2.local>
2015-07-16 16:59:48 -0400

    wrote pseudo code

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// HINT:
+//
+// When you press Enter to submit a new cash register entry, the form will 
+// actually submit data and reload the page. You don't want to reload the page. It 
+// messes up the running tab. So in order to prevent normal form submission, 
+// make sure your form submit handler looks like this:
+//
+	// $('#entry').submit(function(e) {
+	// 		e.preventDefault();
+	//      .... do other stuff here ....
+	// 
+	// });
+
+$(function() {
+	//capture user input
+	//form submit handler so page doesn't reload
+	//convert amount to currency
+	//display input as as a row in the table
+	//add the user input to the total (#total)
+
+)};
+
