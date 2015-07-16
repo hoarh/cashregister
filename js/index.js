4290d77bdd7ade75c2b06af60acf24163cc5b756
hoarh <hoarh@hannahs-MacBook-Pro-2.local>
2015-07-16 18:55:14 -0400

    currency formula

--- js/index.js
+++ js/index.js
@@ -12,11 +12,21 @@
 	// });
 
 $(function() {
-	//capture user input
-	//form submit handler so page doesn't reload
+	
+	$('#entry').submit(function(e) {
+	 	e.preventDefault();
+		var userInput = parseFloat($('#newEntry').val());		
+		userInput = userInput.toFixed(2);
+		console.log(userInput);
+		$('#entries').append('<tr><td></td><td>' + '$' + userInput + '</td></tr>');
+		var total = (total + userInput);
+		$('#total').html('$'+total);
+	});
+
+	//when user clicks enter, form submit handler so page doesn't reload and capture user input
 	//convert amount to currency
 	//display input as as a row in the table
 	//add the user input to the total (#total)
 
-)};
+});
 
